6e08da7c02ad46baa6c5d0560b3bf05c0306abeb
SanketWagh <firearmz0147@gmail.com>
2022-04-29 08:01:24 +0530

    added template literal:

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/templateString.js
@@ -0,0 +1,14 @@
+let str_ = `this is a template 
+string
+which can 
+span 
+throug multiple lines
+`;
+const age = 10;
+const name = "sanket ";
+const job = "software developer";
+
+template_literal = `My name is ${name} and I am ${age} and I work as a ${job}.`;
+
+console.log(str_);
+console.log(template_literal);
