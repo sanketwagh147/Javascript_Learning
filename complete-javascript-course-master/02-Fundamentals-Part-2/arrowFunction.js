59c317e03d6459e6a89ee6dc5f6176555a8f9902
SanketWagh <firearmz0147@gmail.com>
2022-05-05 09:07:24 +0530

    fucntions and arrow function

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/arrowFunction.js
@@ -0,0 +1,21 @@
+// sample arrow function
+
+const add = (a, b) => a + b;
+
+console.log(add(1, 2));
+
+// function declarationn
+
+function sub(a, b) {
+	return a - b;
+}
+
+console.log(sub(5, 3));
+
+//function expressions
+
+const mul = function (a, b) {
+	return a * b;
+};
+
+console.log(mul(4, 2));
